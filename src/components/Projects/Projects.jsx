0624c6c72bb9af27f565d0a7cd57d22189517298
Jimmy-legg <Jimmy.legg18@gmail.com>
2024-11-13 22:31:49 +0100

    adding new projects cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,6 +5,7 @@ const Projects = () => {
   const projects = [
     {
       title: "Python Snake",
+      subtitle: "school project",
       description: "This project was made in high school, it is one of my first in python. It's a replica of the game called Snake. I coded this game using Python and the library PyGame to display it. In this version you can choose between 3 levels and 3 speeds for each the score is saved in a file.",
       link: "https://github.com/Leteno18/Snake",
       className: "a"
@@ -18,18 +19,32 @@ const Projects = () => {
     },
     {
       title: "Java Project",
+      subtitle: "school project",
       description: "In this project, we had to create the java replica of the board game named Orchard for university. This game was created in groupe of 2. In this game you have to roll a dice to collect 40 fruits, you lose if you roll 9 time the crow and place all 9 pieces of a the puzzle. This game includes saving player data, music, a rules section and option section. For this project, we had to do versions of the game on git.",
       link: "https://github.com/Jimmy-Legg/Orchard",
       className: "c"
     },
     {
-      title: "Current project",
-      description: "Creating an app for Le Grand to manage data using MySQL nodeJS React. I cannot reveal any more details due to confidentiality.",
+      title: "Legrand Project",
+      subtitle: "school project",
+      description: "Creating an app for Legrand to manage data using MySQL, Node.js, and React. This project involves developing a comprehensive data management solution for a leading electrical and digital building infrastructure company.",
       className: "d"
     },
     {
-      title: "Resume",
-      description: "Here you can find my Resume",
+      title: "Dreams Donuts Command",
+      subtitle: "personal project",
+      description: "Explore the Dreams Donuts Command project, a web-based application designed to streamline donut production and management in a multi-level donut establishment.",
+      link: "/DreamsDonuts",
+      className: "f"
+    },
+    {
+      title: "Current Project: La Poste RAG",
+      description: "Developing a Retrieval-Augmented Generation (RAG) system for La Poste. This cutting-edge project combines advanced natural language processing techniques to enhance information retrieval and generation capabilities for France's postal service.",
+      className: "g"
+    },
+    {
+      title: "CV",
+      description: "Here you can find my CV",
       link: "pdf/Jimmy Legg.pdf",
       className: "e"
     }
